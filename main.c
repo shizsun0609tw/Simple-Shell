82904a9a0cd3c92d657249079e0041809c961fb8
zhchen0609 <zhchen0609@nplinux7.cc.cs.nctu.edu.tw>
2020-10-11 15:28:56 +0800

    init shell

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void ShellMainLoop()
+{
+	while(1)
+	{
+		printf("%% ");
+		
+		char *buffer;
+		size_t index = 0;
+		
+		getline(&buffer, &index, stdin);
+	
+		if(buffer[0] == '0')
+		{
+			break;
+		}
+	}		
+}
 
 int main(int argc, char ** argv, char ** envp)
 {
-	printf("Hello Linux\n");
+	printf("Hello Shell\n");
+
+	ShellMainLoop();	
 
 	return 0;
 }
